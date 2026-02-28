ff9d676aeb24327f407143833a42ce671ffbe0c2
Christopher Nicholson <nicholsonc@gmail.com>
2026-02-28 18:11:01 -0500

    factorial example added

--- cop2271/more_functions/factorial.c
+++ cop2271/more_functions/factorial.c
@@ -1,8 +1,22 @@
 #include<stdio.h>
 
+int factorial(int num);
+
 int main(void)
 {
-    /* CODE GOES HERE */
+    int num;
+    
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    printf("%d! = %d\n", num, factorial(num));
 
     return 0;
+}
+
+int factorial(int num)
+{
+    if(num <= 1) return 1;
+
+    return num * factorial(num - 1);
 }

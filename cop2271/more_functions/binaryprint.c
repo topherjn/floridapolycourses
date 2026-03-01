99fa5cd0e43cf87ee7e3dae5a6c50b4d456343fe
Christopher Nicholson <nicholsonc@gmail.com>
2026-02-28 20:32:22 -0500

    Added prompt for arbitrary input to convert to binary.

--- cop2271/more_functions/binaryprint.c
+++ cop2271/more_functions/binaryprint.c
@@ -4,8 +4,12 @@ void print_binary(int n);
 
 int main(void)
 {
+    int decimal_number = 0;
+
+    printf("Enter a decimal number to convert to binary: ");
+    scanf("%d", &decimal_number);
     
-    print_binary(13);
+    print_binary(decimal_number);
 
     return 0;
 }

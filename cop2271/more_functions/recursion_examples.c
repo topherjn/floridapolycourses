f70a0f5260f41f3344a3dce23242ca98d7d7717c
Christopher Nicholson <nicholsonc@gmail.com>
2026-03-01 09:13:20 -0500

    Cantor set program created.

--- cop2271/more_functions/recursion_examples.c
+++ cop2271/more_functions/recursion_examples.c
@@ -2,6 +2,7 @@
 
 void reverse_print(char str[], int index);
 int count_paths(int rows, int cols);
+int peasant_multiply(int a, int b, int accumulator);
 
 
 int main(void)
@@ -12,6 +13,12 @@ int main(void)
 
 
     printf("\n%d\n", count_paths(4,5));
+
+    printf("%d\n", peasant_multiply(7,3,0));
+
+    int arr[10];
+
+    cantor(arr, 0, 10 );
 
     return 0;
 }
@@ -31,5 +38,11 @@ int count_paths(int rows, int cols) {
     printf("Tracing row %d, column %d\n", rows, cols);
 
     return count_paths(rows - 1, cols) + count_paths(rows, cols - 1);
+}
+
+int peasant_multiply(int a, int b, int accumulator) {
+    if (a == 0) return accumulator;
+    if (a % 2 != 0) accumulator += b;
+    return peasant_multiply(a / 2, b * 2, accumulator);
 }
 

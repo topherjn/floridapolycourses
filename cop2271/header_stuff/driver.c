88a4c524e593115c9d15d0d644fbfefe9a6e1f6e
Christopher Nicholson <nicholsonc@gmail.com>
2026-03-05 09:53:34 -0500

    randomization done

--- cop2271/header_stuff/driver.c
+++ cop2271/header_stuff/driver.c
@@ -1,14 +1,33 @@
 #include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
 #include"mysort.h"
+
+void print_array(int *array, int size);
 
 int main(void)
 {
-    int array[10] = {3,5,1,7,8,9,4,5,3,6};
+    srand((unsigned int) time(NULL));
+
+
+    int array[10];
+
+    for(int i=0; i<10;i++) array[i] = rand() % 100 + 1;
+
+    printf("Unsorted array: ");
+    print_array(array, 10);
 
     sort(array, 10);
 
-    for(int i = 0; i < 10; i++) 
-        printf("%d ", array[i]);
+    printf("Sorted array: ");
+    print_array(array, 10);
 
     return 0;
+}
+
+void print_array(int *array, int size)
+{
+    for(int i = 0; i < 10; i++) 
+        printf("%d ", array[i]);
+    printf("\n");
 }

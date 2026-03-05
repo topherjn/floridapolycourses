61f5923e061206865bb77ee826c44d3f022012ec
Christopher Nicholson <nicholsonc@gmail.com>
2026-03-05 09:55:50 -0500

    comments added to sort example

--- cop2271/header_stuff/driver.c
+++ cop2271/header_stuff/driver.c
@@ -3,22 +3,26 @@
 #include<time.h>
 #include"mysort.h"
 
+/* functin to print array */
 void print_array(int *array, int size);
 
 int main(void)
 {
     srand((unsigned int) time(NULL));
 
-
     int array[10];
 
+    /* initialize array with random integers, 1 to 100 */
     for(int i=0; i<10;i++) array[i] = rand() % 100 + 1;
 
+    /* show array before sort */
     printf("Unsorted array: ");
     print_array(array, 10);
 
+    /* call the sorting procedure */
     sort(array, 10);
 
+    /* show the successful sort */
     printf("Sorted array: ");
     print_array(array, 10);
 

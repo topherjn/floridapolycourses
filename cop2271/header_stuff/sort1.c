0d8821e1be96784be076efb9d918f003472d8084
Christopher Nicholson <nicholsonc@gmail.com>
2026-03-10 09:52:23 -0400

    number_of_passes added to bubblesort

--- cop2271/header_stuff/sort1.c
+++ cop2271/header_stuff/sort1.c
@@ -1,12 +1,15 @@
 #include<stdio.h>
 #include "mysort.h"
 
+int number_of_passes = 0;
+
 void sort(int *numbers, int size)
 {
     for(int i = 0; i < size - 1; i++)
     {
         for(int j = 0; j < size - i - 1; j++) 
         {
+            number_of_passes++;
             if(numbers[j] > numbers[j+1])
             {
                 int temp = numbers[j];

c07e408748142095109a6dfb28bc2240db9cf761
Christopher Nicholson <nicholsonc@gmail.com>
2026-03-17 18:01:30 -0400

    traverse floats and print out

--- cop2271/arrays/basic_array.c
+++ cop2271/arrays/basic_array.c
@@ -3,19 +3,25 @@
 
 int main(void)
 {
+    /* declare an array of integers */
     int num_array[5];
 
+    /* assign values to the elements */
     num_array[0] = 23;
     num_array[1] = -45;
     num_array[2] = 100;
     num_array[3] = (int) (pow(2, 31) - 1);
     num_array[4] = 0;
 
+    /* print out one element */
     printf("%d\n", num_array[3]);
 
+    /* use an array initializer to populate with floats */
     float measurements[5] = {2.3,-1.0,3,3.14, -10.1};
 
-    for(int i = 0; i < 5; i++) printf("%.1f\n", measurements[i]);
+    /* traverse, the array, printout out each value */
+    for(int i = 0; i < 5; i++) 
+        printf("num_array[%d] == %.1f\n", i, measurements[i]);
 
     return 0;
 }

819004b11187aaeb7ebdda60ed1cd704a442349f
Christopher Nicholson <nicholsonc@gmail.com>
2026-03-26 10:17:49 -0400

    Printed out memory addresses of arrays.

--- cop2271/arrays/inclass_arrays.c
+++ cop2271/arrays/inclass_arrays.c
@@ -3,7 +3,23 @@
 
 int main(void)
 {
-    
+    const int ARRAY_SIZE = 5;
+
+    int num_array[ARRAY_SIZE];
+
+    num_array[0] = 23;
+    num_array[1] = -45;
+    num_array[2] = 51;
+    num_array[3] = (int) (pow(2, 31) - 1);
+    num_array[4] = 1;
+
+
+   
+    printf("%p\n", num_array);
+    printf("%p\n", &num_array[0]);
+
+    printf("%p\n", &num_array[3]);
+
      
     return 0;
 }

5d890c0afce002e1c50f9fc01fd70415b6ee04ef
Christopher Nicholson <nicholsonc@gmail.com>
2026-03-31 12:10:29 -0400

    static array example created

--- cop2271/arrays/static_array.c
+++ cop2271/arrays/static_array.c
@@ -1,8 +1,34 @@
-#include<stdio.h>
+#include <stdio.h>
+
+int *demo_static_array();  
+void print_array(int[], int);
 
 int main(void)
 {
-    /* CODE GOES HERE */
+    int *some_array = demo_static_array(); 
+
+    print_array(some_array, 3);
+
+    some_array = demo_static_array();  
+
+    print_array(some_array, 3);
 
     return 0;
+}
+
+int *demo_static_array()
+{
+    static int a_static_array[] = {10, 20, 30};
+
+    a_static_array[0]++;
+
+    return a_static_array;
+}
+
+void print_array(int array[], int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        printf("demo_array[%d] = [%d]\n", i, array[i]);
+    }
 }

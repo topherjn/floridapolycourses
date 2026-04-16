b36bb7261b54e36762f68ef4347acac281e501d1
Christopher Nicholson <nicholsonc@gmail.com>
2026-04-16 15:39:53 -0400

    final touch in class

--- cop2271/structures/structures.c
+++ cop2271/structures/structures.c
@@ -1,10 +1,17 @@
 #include<stdio.h>
+#include<string.h>
 
 struct fraction
 {
     int numerator;
     int denominator;
 };
+
+typedef struct student
+{
+    char name[10];
+    double gpa;
+} STUDENT_T;
 
 int main(void)
 {
@@ -15,6 +22,16 @@ int main(void)
     half.denominator = 2;
 
     printf("%d/%d\n", half.numerator, half.denominator);
+
+    STUDENT_T s;
+    strcpy(s.name,"Paul");
+    s.gpa = 3.95;
+
+    printf("%s %.3f\n", s.name, s.gpa);
+
+    struct student *p = &s;
+
+    printf("%s %.3f\n", p->name, p->gpa);
 
     return 0;
 }

1cb735357b38cd68e35b60639971dbcbaddd2134
Christopher Nicholson <nicholsonc@gmail.com>
2026-04-19 11:28:53 -0400

    several string examples created

--- cop2271/strings/strings.c
+++ cop2271/strings/strings.c
@@ -1,8 +1,41 @@
 #include<stdio.h>
+#include<string.h>
 
 int main(void)
 {
-    /* CODE GOES HERE */
+    /* canonical C string */
+    char word[20];
+    word[0]='\0';
+
+    strcpy(word,"Hello, strings.");
+
+    printf("%s\n", word);
+
+    printf("Number of characters: %d\n",strlen(word));
+
+    /* this is immutable */
+    char *string = "Bonjour";
+    char *greeting = "Bonjour";
+    char salutation[] = "Bonjour";
+
+    printf("%p %p %p\n", string, greeting, salutation);
+
+    greeting = salutation;
+
+    printf("%p %p %p\n", string, greeting, salutation);
+
+    printf("%s has %d letters in it\n", string, strlen(string));
+
+    for(int i = 0; i < strlen(word); i++)
+    {
+        if(word[i] == '.') word[i] = '!';
+        printf("%c ", word[i]);
+        
+    }
+
+    //string[0] = 'T';
+
+    printf("%s", string);
 
     return 0;
 }

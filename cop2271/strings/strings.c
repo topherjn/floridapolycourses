0eca31df542d3a4a1fbd2e98b8bc8d25fe04677d
Christopher Nicholson <nicholsonc@gmail.com>
2026-04-21 10:38:12 -0400

    added explanatory comments for strings

--- cop2271/strings/strings.c
+++ cop2271/strings/strings.c
@@ -4,28 +4,44 @@
 
 int main(void)
 {
-    /* canonical C string */
+    /* the basis of a C-type string is an array of characters*/
     char word[20];
     
+    /* strcpy will copy the characters of the string literal into
+       the character array */
     strcpy(word,"Hello, strings.");
+    /* strcpy (and many other string.h functions) will place
+       the nul termination character ('\0' aka ASCII 0) in the correct
+       place */
 
+    /* percent s is the placeholder for strings */
     printf("%s\n", word);
 
+    /* strlen tells you how many characters are in the string,
+       not counting the nul termination character */
     printf("Number of characters: %d\n",strlen(word));
 
-    /* this is immutable */
+    /* these are immutable */
     char *string = "Bonjour";
     char *greeting = "Bonjour";
+    /* and they will point to the same address in the 
+       data segment */
+    /* this is mutable */
     char salutation[] = "Bonjour";
 
+    /* proof of the above */
     printf("%p %p %p\n", string, greeting, salutation);
 
+    /* the pointers themselves can be assigned to other 
+       char addresses (i.e. other strings in this case) */
     greeting = salutation;
 
+    /* proof of the above */
     printf("%p %p %p\n", string, greeting, salutation);
 
     printf("%s has %d letters in it\n", string, strlen(string));
 
+    /* demonstration that char array word is mutable */
     for(int i = 0; word[i] != '\0'; i++)
     {
         if(word[i] == '.') word[i] = '!';
@@ -33,19 +49,28 @@ int main(void)
         
     }
 
+    /* the string called "string" is NOT mutable,
+       and attempting the following as unpredictable 
+       results */
     //string[0] = 'T';
 
     printf("%s", string);
 
+    /* strcat is how you concatenate (literally: "chaing together")*/
     char combine[20] = "concat";
     strcat(combine, "enate");
 
     printf("\n%s\n", combine);
 
+    /* strdup is not standard C, but is often available 
+       unlike the other string functions, it creates space
+       for the string is it assigned */
     char *dupe = strdup("Hello, world!");
 
     printf("%s, %d\n", dupe, strlen(dupe));
 
+    /* but the created space is dynamic and should be freed
+       when you're done using it */
     free(dupe);
 
     return 0;

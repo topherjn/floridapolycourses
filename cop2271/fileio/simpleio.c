bf601689bf9e8864e14a2450d36eef367a9882a8
Christopher Nicholson <nicholsonc@gmail.com>
2026-04-22 14:42:29 -0400

    Changed program to be interactive

--- cop2271/fileio/simpleio.c
+++ cop2271/fileio/simpleio.c
@@ -1,22 +1,24 @@
 #include <stdio.h>
-
+ 
 int main(void) {
     char buf[256];
-    int i;
-
-    /* Read */
+    char newtext[256];
+ 
+    /* Read and display */
     FILE *fp = fopen("sample.txt", "r");
     fgets(buf, sizeof(buf), fp);
     fclose(fp);
-
-    /* Modify */
-    for (i = 0; buf[i]; i++)
-        if (buf[i] == 'h') buf[i] = 'H';
-
-    /* Write back */
-    fp = fopen("sample.txt", "w");
-    fprintf(fp, "%s", buf);
+    printf("File contains: %s\n", buf);
+ 
+    /* Prompt user for text to append */
+    printf("Enter text to append: ");
+    fgets(newtext, sizeof(newtext), stdin);
+ 
+    /* Append to file */
+    fp = fopen("sample.txt", "a");
+    fprintf(fp, "%s", newtext);
     fclose(fp);
-
+ 
+    printf("Done.\n");
     return 0;
 }

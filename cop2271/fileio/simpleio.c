f137ccfe20ee29736e6102b72e9d319a086fe9a2
Christopher Nicholson <nicholsonc@gmail.com>
2026-04-23 10:49:20 -0400

    Changed simple File io again

--- cop2271/fileio/simpleio.c
+++ cop2271/fileio/simpleio.c
@@ -2,32 +2,19 @@
 #include <stdlib.h>
 
 int main(void) {
-    // Write a file
-    FILE *fp = fopen("sample.txt", "w");
+    FILE *fp = fopen("data.txt", "r");
     if (fp == NULL) {
-        perror("Could not open file for writing");
+        perror("fopen failed");
         return EXIT_FAILURE;
     }
 
-    for (int i = 1; i <= 5; i++) {
-        fprintf(fp, "Line %d\n", i);
-    }
-    fclose(fp);
-
-    // Read it back
-    fp = fopen("sample.txt", "r");
-    if (fp == NULL) {
-        perror("Could not open file for reading");
-        return EXIT_FAILURE;
-    }
-
-    char buffer[64];
+    char buffer[256];
     while (fgets(buffer, sizeof(buffer), fp) != NULL) {
         printf("%s", buffer);
     }
 
     if (ferror(fp)) {
-        perror("Read error");
+        perror("read error");
     }
 
     fclose(fp);

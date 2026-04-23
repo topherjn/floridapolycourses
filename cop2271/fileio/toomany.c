7498e1e48b1044bf00cc3d4f262daaed350df7a4
Christopher Nicholson <nicholsonc@gmail.com>
2026-04-23 10:25:10 -0400

    Added too many example

---
+++ cop2271/fileio/toomany.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void) {
+    FILE *fp;
+    int count = 0;
+
+    while (!0) {
+        fp = fopen("sample.txt", "r");
+        count++;
+        if (fp == NULL) {
+            printf("fopen failed at call #%d\n", count);
+            break;
+        }
+        printf("Call #%d -> FILE* address: %p | fd: %d\n", count, (void*)fp, _fileno(fp));
+    }
+
+    return 0;
+}

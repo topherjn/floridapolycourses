843299995075ff4f988c7ec1f6bbc7bfb5946976
Christopher Nicholson <nicholsonc@gmail.com>
2026-04-25 21:09:37 -0400

    strcmp added

---
+++ cop2271/strings/strcmp.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int strcmp(const char *s1, const char *s2);
+
+int main(void)
+{
+
+    char *word1 = "apple";
+    char *word2 = "apple";
+
+    printf("\n%d\n", strcmp(word1,word2));
+
+    return 0;
+}
+
+int strcmp(const char *s1, const char *s2) {
+    while (*s1 && (*s1 == *s2)) {
+        s1++;
+        s2++;
+    }
+    printf("%p|%p", s1,s2);
+    return *(unsigned char *)s1 - *(unsigned char *)s2;
+}
